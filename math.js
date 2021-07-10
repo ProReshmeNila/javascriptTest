2fd462b95e5e76238ccb6a95cc734bf4ce0952b0
ProReshmeNila <developernila2021@gmail.com>
2021-07-11 02:40:01 +0600

    text

--- math.js
+++ math.js
@@ -2,5 +2,8 @@ var x = 10.5;
 var y = 20;
 var result = Math.floor(x);// floor value er kom nibe
 var result = Math.ceil(x);// ceil value er besi nibe
-var result = Math.round(x);// round .5 er kom thakle kom nibe .5 er besi thakle  besi nibe
+var result = Math.round(x);// round .5 er kom thakle kom nibe .5 er besi thakle  besi nibe. full number ta nibe . .000 er por jai thakuk nibe na
 console.log(result)
+var lottery = Math.random() * 100; // random kono value newar jonno
+var winner = Math.round(lottery);// 1.22 point sara newar jonno.
+console.log(winner);
