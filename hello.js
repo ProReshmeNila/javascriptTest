1ba80804e31b1c3d7c55833c0a37f67be7d939d2
ProReshmeNila <developernila2021@gmail.com>
2021-07-11 02:34:37 +0600

    math

--- hello.js
+++ hello.js
@@ -1,2 +1,57 @@
 var name = "Reshma Khanam";
 console.log(name);
+console.log(typeof name);
+var age = 25;
+console.log(age);
+console.log(typeof age);
+var busVara = 25.50;
+console.log(busVara);
+console.log(typeof busVara);
+
+var taka = 100;
+var poisa = 50;
+console.log(taka + poisa);
+console.log(taka / poisa);
+console.log(taka * poisa);
+console.log(taka - poisa);
+
+var numf = 25.5;
+console.log(typeof numf);
+var dump = numf;
+var name = "humasha";
+console.log(name);
+var e = 200;
+console.log(e);
+var a = '10.2';
+var b = '10.5';
+b = parseFloat(b);
+a = parseInt(a);
+console.log(a + b);
+var y = 15;
+var u = 10;
+console.log(y % u);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ math.js
@@ -0,0 +1,6 @@
+var x = 10.5;
+var y = 20;
+var result = Math.floor(x);// floor value er kom nibe
+var result = Math.ceil(x);// ceil value er besi nibe
+var result = Math.round(x);// round .5 er kom thakle kom nibe .5 er besi thakle  besi nibe
+console.log(result)
