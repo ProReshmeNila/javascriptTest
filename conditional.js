707802769c5f38a13e8cde954fe09336bc05eb32
ProReshmeNila <developernila2021@gmail.com>
2021-07-11 22:06:52 +0600

    conditional js

---
+++ conditional.js
@@ -0,0 +1,49 @@
+// conditional js
+// && means and, 2 tai sotti hote hobe. 
+var jobPaiso = true;
+var savingAmount = 50000;
+if (jobPaiso==true && savingAmount>2000) { 
+    console.log('you can get marry');       
+} else {
+    console.log('biya kora lagbe na')
+}
+// || means or , j kono akta sotti hote hobe
+var bariJabo = false;
+var takaLagbe = 1000;
+if (bariJabo==false || takaLagbe>500) { 
+    console.log('Bari jety parba');
+} else {
+    console.log('taka nai ');
+}
+
+// == means value er man soman hote hobe. 
+var x = 300;
+if (x==30) {
+    console.log('you can go');
+} else {
+    console.log('you can not go');
+}
+// != means value er man soman noy. 
+var b = 300;
+if (b != 3000) {
+    console.log('you can go');
+} else {
+    console.log('you can not go');
+}
+//    >= means value soman ba tar boro
+var y = 300;
+if (y >= 3000) {
+    console.log('you can go');
+} else {
+    console.log('you can not go');
+}
+
+//    <= means value soman ba tar soto
+
+var a = 3000;
+if (a <= 3000) {
+    console.log('you can go');
+} else {
+    console.log('you can not go');
+}
+
